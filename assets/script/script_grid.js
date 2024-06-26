a8e1eff4136d70949cda41fdf2d2ef82d1f53a30
GonzaSegu <58949727+GonzaSegu@users.noreply.github.com>
2024-06-26 00:31:57 -0400

    Add files via upload

---
+++ assets/script/script_grid.js
@@ -0,0 +1,52 @@
+const hamburguesa = document.getElementById('burguer');
+const menuFalso = document.getElementById('menuFalso');
+const menu = document.getElementById('menu');
+const foto_team = document.getElementById('team');
+const foto_servicios = document.getElementById('foto_servicios');
+
+const titulos  = document.querySelectorAll('h4');
+const parrafos = document.querySelectorAll('p');
+const comentarios = document.querySelectorAll('h6')
+
+if (window.innerWidth < 576){
+    menu.style.display = 'none';
+    foto_team.style.display='none'
+    foto_servicios.style.display ='none';
+
+} else{
+    hamburguesa.style.display = 'none';
+    menuFalso.style.display = 'none';
+    /*titulos.forEach(titulo=>{
+        let newTitulo = document.createElement('h2');
+        newTitulo.innerHTML=titulo.innerHTML;
+        titulo.parentNode.replaceChild(newTitulo, titulo);
+    })
+    parrafos.forEach(parrafo=>{
+        let newParrafo = document.createElement('h5');
+        newParrafo.innerHTML=parrafo.innerHTML;
+        newParrafo.style.margin = '0px';
+        newParrafo.style.textAlign = 'justify'
+        parrafo.parentNode.replaceChild(newParrafo, parrafo);
+    })
+    comentarios.forEach(comentario=>{
+        let newComment = document.createElement('h5');
+        newComment.innerHTML=comentario.innerHTML;
+        comentario.parentNode.replaceChild(newComment, comentario);
+    })  */
+}
+
+hamburguesa.addEventListener('click', function() {
+    if (menu.style.display === 'none' || menu.style.display === '') {
+        menuFalso.style.display = 'none';
+        menu.style.display = 'block';
+       
+    } else {
+        menuFalso.style.display = 'block';
+        menu.style.display = 'none';
+    }
+})
+
+
+
+
+
